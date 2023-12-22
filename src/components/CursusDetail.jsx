1b093470e40895ec42bd9c42ded104ca6dc07c80
Suzanne Boersma <suzanneboersma@outlook.com>
2023-12-22 08:36:37 +0100

    CursusDetail.jsx

---
+++ src/components/CursusDetail.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Box, Heading, VStack, Image, Text } from '@chakra-ui/react';
+
+const CursusDetail = ({ course, onSelect }) => {
+  if (!course) {
+    return null;
+  }
+
+  const {
+    title,
+    description,
+    image,
+    startTime,
+    endTime,
+    categories,
+    instructor: { name: instructorName, image: instructorImage },
+  } = course;
+
+  return (
+    <Box>
+      <Heading fontSize='xl' mb={2}>
+        {title}
+      </Heading>
+      <VStack align="start" spacing={2}>
+        <Box>
+          <strong>Description:</strong> {description}
+        </Box>
+        <Box>
+          <strong>Start Time:</strong> {startTime}
+        </Box>
+        <Box>
+          <strong>End Time:</strong> {endTime}
+        </Box>
+        <Box>
+          <strong>Categories:</strong> {categories.join(', ')}
+        </Box>
+        <Box>
+          <strong>Instructor:</strong>
+          {instructorName} {instructorImage && (
+            <img src={instructorImage} alt={`Image for ${instructorName}`} style={{ maxWidth: '50px' }} />
+          )}
+          <Link to={`/event/${course.id}`}>
+        <Button colorScheme='blue' mt={2} onClick={onSelect}>
+          Bekijk details
+        </Button>
+      </Link>
+        </Box>
+      </VStack>
+    </Box>
+  );
+};
+
+export default CursusDetail;
