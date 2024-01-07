16633872607381c61da5a09056ea8cc6c7e32eea
Suzanne Boersma <suzanneboersma@outlook.com>
2024-01-08 00:05:13 +0100

    Update7

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+     
     },
-    build: {
-      target: 'esnext',
-    },
-    esbuild: {
-      jsxFactory: 'h',
-      jsxFragment: 'Fragment',
-    },
+  },
+  build: {
+    target: 'esnext',
+  },
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
   },
 });
 
