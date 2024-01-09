42d73295c6d7147e3af139e8d84d72041075e041
Suzanne Boersma <suzanneboersma@outlook.com>
2024-01-09 13:45:55 +0100

    commit message

--- src/components/CourseDetail.jsx
+++ src/components/CourseDetail.jsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { useEffect, useState } from 'react';
 import { Box, Image, VStack, Text, HStack, Button } from '@chakra-ui/react';
-import styles from '../pages/StylePage';
 import { TimeIcon } from '@chakra-ui/icons';
 import { Link, useParams } from 'react-router-dom';
+import styles from '../pages/StylePage';
 
 export const CourseDetail = () => {
   const { courseId } = useParams();
@@ -55,7 +56,7 @@ export const CourseDetail = () => {
     return <p>Course not found</p>;
   }
 
-  const { title, description, image, categories, startTime, endTime, instructor } = course;
+  const { title, description, image, categories, startTime, endTime, instructor, location, prijs, website, quote } = course;
   const categoriesContent = categories ? categories.join(', ') : 'N/A';
 
   if (!instructor) {
@@ -64,63 +65,64 @@ export const CourseDetail = () => {
 
   return (
     <Link to={`/course/${course.id}`}>
-    <Box maxW="xl" mx="auto" my="4" p="4" borderWidth="1px" borderRadius="lg" overflow="hidden" bgColor="#8AC7DE" boxShadow="lg">
-      <Text fontSize="xl" fontWeight="bold">
-        Geselecteerde Cursus
-      </Text>
-      <HStack spacing="4">
-        <Image src={course.image} alt={course.title} style={{ ...styles.image, marginBottom: '10px' }} />
-        <VStack align="start" spacing="4" flex="1">
-            <Text as="h3" fontSize="lg" fontWeight="bold" color="blue.500" mb={2}>
-              {title}
+      <Box maxW="xl" mx="auto" my="4" p="4" borderWidth="1px" borderRadius="lg" overflow="hidden" bgColor="#8AC7DE" boxShadow="lg">
+        <VStack spacing="4" align="start">
+          <Text fontSize="xl" fontWeight="bold">
+            Geselecteerde Cursus
+          </Text>
+          <Text as="h3" fontSize="lg" fontWeight="bold" color="blue.500" mb={2}>
+            {title}
+          </Text>
+          <Text>
+            <strong>Omschrijving:</strong> {description}
+          </Text>
+          <Box style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Text>
+              <strong>Categorieën:</strong> {categoriesContent}
+            </Text>
+          </Box>
+          <Image src={image} alt={title} style={{ ...styles.image, marginBottom: '10px' }} />
+          <Box>
+            <Text>
+              <TimeIcon marginRight="5px" />
+              <strong>Starttijd:</strong> {startTime}
             </Text>
             <Text>
-              <strong>Omschrijving:</strong> {description}
+              <TimeIcon marginRight="5px" />
+              <strong>Eindtijd:</strong> {endTime}
             </Text>
-            <Box style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Text>
-                <strong>Categorieën:</strong> {categoriesContent}
-              </Text>
-            </Box>
-            <Box>
-              <Text>
-                <TimeIcon />
-                <strong>Starttijd:</strong> {startTime}
-              </Text>
-              <Text>
-                <TimeIcon />
-                <strong>Eindtijd:</strong> {endTime}
-              </Text>
-              <Text>
-                <strong>Locatie:</strong> {course.location}
-              </Text>
-              <Text>
-                <strong>Prijs:</strong> {course.prijs}
-              </Text>
-              <Text>
-                <strong>Website:</strong> {course.website}
-              </Text>
-              <Text>
-                <strong>Quote: </strong> {course.quote}
-              </Text>
-            </Box>
-            <HStack spacing="2">
-              <Text>
-                <strong>Docent: </strong> {course.instructor?.name || 'Informatie niet beschikbaar'}
-              </Text>
-              {course.instructor?.image && (
-                <Image src={course.instructor?.image} alt={course.instructor?.name} style={styles.imageInstrutor} />
-              )}
-            </HStack>
-          </VStack>
-        </HStack>
+            <Text>
+              <strong>Locatie:</strong> {location}
+            </Text>
+            <Text>
+              <strong>Prijs:</strong> {prijs}
+            </Text>
+            <Text>
+              <strong>Website:</strong> {website}
+            </Text>
+            <Text>
+              <strong>Quote: </strong> {quote}
+            </Text>
+          </Box>
+          <HStack spacing="2">
+            <Text>
+              <strong>Docent: </strong> {instructor?.name || 'Informatie niet beschikbaar'}
+            </Text>
+            {instructor?.image && <Image src={instructor.image} alt={instructor.name} style={styles.imageInstrutor} />}
+          </HStack>
+        </VStack>
         <Text fontSize="xl" fontWeight="bold">
           Schrijf je nu in:
         </Text>
-        <Button colorScheme="blue" variant="outline"  mt={2} mr={2} mb={2}>
-          inschrijvingcursus@email.nl
+        <Button colorScheme="blue" variant="outline" mt={2} mr={2} mb={2}>
+          inschrijvingcursus@fakeEmail.nl
         </Button>
       </Box>
     </Link>
   );
 };
+
+export default styles;
+
+
+
